c6e67a8c44093f964ac1d4721f20491098e2ea4c
Vinicius de Souza Vieira <vinicius.svieira8@gmail.com>
2023-11-22 15:50:14 -0300

    Primeira execução do servidor

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const express = require("express")
 
 const app = express()
 
+app.get('/', (requisicao, resposta) =>{
+    resposta.send("Ola, mundo!")
+})
+
+app.listen(3000, () =>{
+    console.log("Servidor rodando na porta 3000!")
+})
