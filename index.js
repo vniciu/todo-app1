02405e8e7bf22852a49a42b55ed7d75316e467a7
Vinicius de Souza Vieira <vinicius.svieira8@gmail.com>
2023-11-24 16:14:43 -0300

    exibição das tarefas na pagina home concluida

--- index.js
+++ index.js
@@ -51,9 +51,8 @@ app.get('/', (requisicao, resposta) =>{
             }
         })
 
+        resposta.render('home', { tarefas })
     })
-
-    resposta.render('home')
 })
 
 const conexao = mysql.createConnection({
