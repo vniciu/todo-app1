2588e5ebfc8cc816b4bf20adc842cc9b04c7cfce
Vinicius de Souza Vieira <vinicius.svieira8@gmail.com>
2023-11-25 13:03:07 -0300

    exclusão de tarefas individuais

--- index.js
+++ index.js
@@ -15,6 +15,23 @@ app.use(express.urlencoded({
 
 app.use(express.json())
 
+app.post('/excluir', (requisicao, resposta) => {
+    const id = requisicao.body.id
+
+    const sql = `
+        DELETE FROM tarefas
+        WHERE id = ${id}
+    `
+
+    conexao.query(sql, (erro) => {
+        if (erro) {
+            return console.log(erro)
+        }
+
+        resposta.redirect('/')
+    })
+})
+
 app.post('/completar', (requisicao, resposta) => {
     const id = requisicao.body.id
 
